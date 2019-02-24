dd404ccfcf8d147c782e118deaf93d5794cd9aad
julio.ospina <julioone@utp.edu.co>
2019-02-24 17:43:54 -0500

    Implementacion con Heuristica de accesibilidad.

--- src/paseocaballo/PaseoCaballo.java
+++ src/paseocaballo/PaseoCaballo.java
@@ -27,6 +27,83 @@ public class PaseoCaballo {
         //columnaActual +- horizontalMov[ numeroMovimiento];
         int [] horizontalMov = new int[8];    
         int [] verticalMov   = new int[8];
+        
+        //heuristica de accesibilidad entre menos valor mas accesible.
+        int [][] tablaAccesibilidad = new int[8][8];
+        tablaAccesibilidad[0][0] = 2;
+        tablaAccesibilidad[0][1] = 3;
+        tablaAccesibilidad[0][2] = 4;
+        tablaAccesibilidad[0][3] = 4;
+        tablaAccesibilidad[0][4] = 4;
+        tablaAccesibilidad[0][5] = 4;
+        tablaAccesibilidad[0][6] = 3;
+        tablaAccesibilidad[0][7] = 2;
+        
+        tablaAccesibilidad[1][0] = 3;
+        tablaAccesibilidad[1][1] = 4;
+        tablaAccesibilidad[1][2] = 6;
+        tablaAccesibilidad[1][3] = 6;
+        tablaAccesibilidad[1][4] = 6;
+        tablaAccesibilidad[1][5] = 6;
+        tablaAccesibilidad[1][6] = 4;
+        tablaAccesibilidad[1][7] = 3;
+        
+        tablaAccesibilidad[2][0] = 4;
+        tablaAccesibilidad[2][1] = 6;
+        tablaAccesibilidad[2][2] = 8;
+        tablaAccesibilidad[2][3] = 8;
+        tablaAccesibilidad[2][4] = 8;
+        tablaAccesibilidad[2][5] = 8;
+        tablaAccesibilidad[2][6] = 6;
+        tablaAccesibilidad[2][7] = 4;
+        
+        tablaAccesibilidad[3][0] = 4;
+        tablaAccesibilidad[3][1] = 6;
+        tablaAccesibilidad[3][2] = 8;
+        tablaAccesibilidad[3][3] = 8;
+        tablaAccesibilidad[3][4] = 8;
+        tablaAccesibilidad[3][5] = 8;
+        tablaAccesibilidad[3][6] = 6;
+        tablaAccesibilidad[3][7] = 4;
+        
+        tablaAccesibilidad[4][0] = 4;
+        tablaAccesibilidad[4][1] = 6;
+        tablaAccesibilidad[4][2] = 8;
+        tablaAccesibilidad[4][3] = 8;
+        tablaAccesibilidad[4][4] = 8;
+        tablaAccesibilidad[4][5] = 8;
+        tablaAccesibilidad[4][6] = 6;
+        tablaAccesibilidad[4][7] = 4;
+        
+        tablaAccesibilidad[5][0] = 4;
+        tablaAccesibilidad[5][1] = 6;
+        tablaAccesibilidad[5][2] = 8;
+        tablaAccesibilidad[5][3] = 8;
+        tablaAccesibilidad[5][4] = 8;
+        tablaAccesibilidad[5][5] = 8;
+        tablaAccesibilidad[5][6] = 6;
+        tablaAccesibilidad[5][7] = 4;
+        
+        tablaAccesibilidad[6][0] = 3;
+        tablaAccesibilidad[6][1] = 4;
+        tablaAccesibilidad[6][2] = 6;
+        tablaAccesibilidad[6][3] = 6;
+        tablaAccesibilidad[6][4] = 6;
+        tablaAccesibilidad[6][5] = 6;
+        tablaAccesibilidad[6][6] = 4;
+        tablaAccesibilidad[6][7] = 3;
+        
+        tablaAccesibilidad[7][0] = 2;
+        tablaAccesibilidad[7][1] = 3;
+        tablaAccesibilidad[7][2] = 4;
+        tablaAccesibilidad[7][3] = 4;
+        tablaAccesibilidad[7][4] = 4;
+        tablaAccesibilidad[7][5] = 4;
+        tablaAccesibilidad[7][6] = 3;
+        tablaAccesibilidad[7][7] = 2;
+        
+        
+      
         horizontalMov[0] = 2;
         horizontalMov[1] = 1;
         horizontalMov[2] = -1;
@@ -54,23 +131,11 @@ public class PaseoCaballo {
                 tableroAjedrez[i][j] = 0;
             }
         }
-        //pintar tablero
-        /*
-        for(int i=0; i< tableroAjedrez.length; i++)
-        {
-            for(int j=0; j<tableroAjedrez[i].length; j++)
-            {
-                System.out.printf("%b ",tableroAjedrez[i][j]);
-            }
-            System.out.println();
-        }
-        */
+        
         //posicion inicial aleatoria caballo
         String stringfilaActual = JOptionPane.showInputDialog("ingrese la fila inicial 0-7");
         String stringcolumnaActual = JOptionPane.showInputDialog("ingrese la columna inicial 0-7");
-       
-        int auxfilaActual    = 0;
-        int auxcolumnaActual = 0;
+
         int filaActual      = Integer.parseInt(stringfilaActual);
         int columnaActual   = Integer.parseInt(stringcolumnaActual);
         
@@ -80,63 +145,74 @@ public class PaseoCaballo {
         
         for(int movimientos =1; movimientos<=64; movimientos++)
         {
-            for(int tipoMov=0; tipoMov < 8; tipoMov++)
+            
+            int mov_escogido=-1;//movimiendo escodigo en cada iteraccion
+            int puntajeFinal = 9;//puntaje maximo 
+            for(int mov= 0;mov < 8; mov++)
             {
-                
-                auxfilaActual = filaActual;
-                auxcolumnaActual = columnaActual;
-                filaActual += verticalMov[tipoMov];
-                columnaActual += horizontalMov[tipoMov];
-                //System.out.printf("%d,%d;", tempfilaActual,tempcolumnaActual);
-                if ((filaActual < 0 || filaActual > 7) || (columnaActual < 0 || columnaActual > 7))
+                if ((filaActual+verticalMov[mov] < 0 || filaActual+verticalMov[mov] > 7) || (columnaActual+horizontalMov[mov] < 0 || columnaActual+horizontalMov[mov] > 7))
                 {
-                    filaActual = auxfilaActual;
-                    columnaActual = auxcolumnaActual;
-
-                }
-                else 
-                {
+                    //el posible movimiento se sale del tablero.;
                     
-                    if (tableroAjedrez[filaActual][columnaActual] != 0)
+                }
+                else
+                {
+                    //tambien preguntar si la casilla no ha sido visitada.
+                    if(tableroAjedrez[filaActual+verticalMov[mov]][columnaActual+horizontalMov[mov]] != 0)
                     {
-                        //ya esta visitado
-                        //no muevo el caballo y sigo con el posible proximo movimiento.
-                        filaActual = auxfilaActual;
-                        columnaActual = auxcolumnaActual;
-
+                        //tampoco sirve un movimiento a una casilla ya visitada.
+                        
                     }
                     else
                     {
-                        //desplazo el caballo.
-
-
-                        tableroAjedrez[filaActual][columnaActual] = 1; //visitado.
-                        System.out.printf("Caballo se movio a [%d][%d]\n",filaActual,columnaActual);
-                        for(int i=0; i< tableroAjedrez.length; i++)
+                        //aqui si se toma como un movimiento valido y compite con otros para escoger el mejor movimiento.
+                        int puntajeActual = tablaAccesibilidad[filaActual+verticalMov[mov]][columnaActual+horizontalMov[mov]];
+                        if(puntajeActual < puntajeFinal)
                         {
-                            for(int j=0; j<tableroAjedrez[i].length; j++)
-                                {
-                                    System.out.printf("%d ",tableroAjedrez[i][j]);
-                            }
-                            System.out.println();
+                            puntajeFinal = puntajeActual;
+                            mov_escogido = mov;
                         }
-                        contMovExitosos++;
-                        //System.out.printf(":%d:",contMovExitosos);
-                        break;
-
+                        
                     }
-
-                }//visitada = tableroAjedrez[tempfilaActual][tempcolumnaActual];
+                    //
+                    
+                    
+                }
+            }
+            //el movimiento ya esta escogido.
+            
+            
                 
+                
+                
+                
+                
+            
+            if(mov_escogido == -1)
+            {
+                //ya el movimiento del caballo no tiene salida.
                 
                 
             }
-            
-            
+            else
+            {
+                //mover caballado a nueva casilla
+                filaActual    += verticalMov[mov_escogido];
+                columnaActual += horizontalMov[mov_escogido];
+                tableroAjedrez[filaActual][columnaActual] = 1; //visitado.
+                
+                for(int i=0; i< tableroAjedrez.length; i++)
+                    {
+                        for(int j=0; j<tableroAjedrez[i].length; j++)
+                            {
+                                System.out.printf("%d ",tableroAjedrez[i][j]);
+                            }
+                        System.out.println();
+                    }
+                System.out.printf("Caballo se movio a [%d][%d]\n",filaActual,columnaActual);
+                contMovExitosos++;
+            }
         }
-        System.out.printf("el numero de movimientos exitosos fueron %d\n",contMovExitosos);
-        
-        
+        System.out.printf("el numero de mov fueron %d ", contMovExitosos);
     }
-    
 }
